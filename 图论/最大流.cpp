410f94b00617e21be4e2506f9e56930edeb44ee9
Li Si <diguo@stud.tjut.edu.cn>
2024-11-18 23:50:53 +0800

    Update and rename 网络流.cpp to 最大流.cpp

--- 图论/最大流.cpp
+++ 图论/最大流.cpp
@@ -1,18 +1,20 @@
-最大流,同时也是最小割
-
-
-如果需要记录最终每条边的流量, 需要在edge里加一项bool, 记录是否为正向边
-最终, 每条边流量是当前的c
-
+/*
+v表示出边, rev表示反向边为e[v][rev]
+c表示是网络流的最大流量, flow表示当前
+fwd表示是是否是正向边, 便于统计
+*/
 struct edge
 {
     int v, rev;
-    ll c;
+    ll c, flow;
+    bool fwd;
 };
 
-要传引用,否则复杂度会加个n
-INF和LNF别弄混了
-ll max_flow(int s, int t, vector<vector<edge> > &e)
+/*
+如果WA了，检查：
+是否清空时候没清0, INF是否错设为LNF
+*/
+ll max_flow(int s, int t, vector<vector<edge>> &e)
 {
     int n = e.size();
     vector<int> dep(n + 1);
@@ -30,8 +32,8 @@ ll max_flow(int s, int t, vector<vector<edge> > &e)
             int m = e[u].size();
             rep(i, 0, m - 1)
             {
-                auto [v, rev, c] = e[u][i];
-                if (dep[v] == INF && c)
+                auto &[v, rev, c, flow, fwd] = e[u][i];
+                if (dep[v] == INF && flow < c)
                 {
                     dep[v] = dep[u] + 1;
                     q.push(v);
@@ -47,12 +49,12 @@ ll max_flow(int s, int t, vector<vector<edge> > &e)
         int m = e[u].size();
         for (int i = cur[u]; i < m && now; ++i)
         {
-            auto [v, rev, c] = e[u][i];
-            if (!c || dep[v] != dep[u] + 1) continue;
+            auto &[v, rev, c, flow, fwd] = e[u][i];
+            if (flow >= c || dep[v] != dep[u] + 1) continue;
             cur[u] = i;
-            ll f = dfs(v, min(now, c));
-            e[u][i].c -= f;
-            e[v][rev].c += f;
+            ll f = dfs(v, min(now, c - flow));
+            e[u][i].flow += f;
+            e[v][rev].flow -= f;
             now -= f;
         }
         return flow - now;
@@ -62,10 +64,9 @@ ll max_flow(int s, int t, vector<vector<edge> > &e)
     return ans;
 }
 
-加边函数在solve内写
-注意,反向边初始流量为0, 打完检查一遍,别打错了
+//母式化加边
 auto addedge = [&](int u, int v, ll c)
 {
-    e[u].pb({v, (int)e[v].size(), c});
-    e[v].pb({u, (int)e[u].size() - 1, 0});
+    e[u].pb({v, (int)e[v].size(), c, 0, true});
+    e[v].pb({u, (int)e[u].size() - 1, 0, 0, false});
 };
