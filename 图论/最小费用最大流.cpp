720e67b3cc5deb9ce6ac97fba4092d17cdca4e02
Li Si <diguo@stud.tjut.edu.cn>
2024-11-18 23:51:29 +0800

    Update 最小费用最大流.cpp

--- 图论/最小费用最大流.cpp
+++ 图论/最小费用最大流.cpp
@@ -1,13 +1,11 @@
-如果需要记录最终每条边的流量, 需要在edge里加一项bool, 记录是否为正向边
-最终, 每条边流量是当前的c
-c是容量, w是费用
 struct edge
 {
     int v, rev;
-    ll c, w;
+    ll c, w, flow;
+    bool fwd;
 };
 
-返回pair<ll, ll>: [最大流, 最大费用]
+// 返回pair<ll, ll>: [最大流, 最大费用]
 pll max_flow(int s, int t, vector<vector<edge> > &e)
 {
     int n = e.size();
@@ -18,7 +16,6 @@ pll max_flow(int s, int t, vector<vector<edge> > &e)
     function<bool()> spfa = [&]() -> bool
     {
         queue<int> q;
-        若求最大费用,则改为"dis[i] = -LNF, cur[i] = 0"
         rep(i, 0, n) dis[i] = LNF, cur[i] = 0;
         q.push(s);
         dis[s] = 0;
@@ -31,17 +28,15 @@ pll max_flow(int s, int t, vector<vector<edge> > &e)
             int m = e[u].size();
             rep(i, 0, m - 1)
             {
-                auto [v, rev, c, w] = e[u][i];
-                若求最大费用,这里改为'<'
-                if (dis[v] > dis[u] + w && c)
+                auto &[v, rev, c, w, flow, fwd] = e[u][i];
+                if (dis[v] > dis[u] + w && c > flow)
                 {
                     dis[v] = dis[u] + w;
-                    if (!book[v]) q.push(v), book[v] = 1;
+                    if (!book[v]) { q.push(v); book[v] = 1; }
                 }
             }
         }
         return dis[t] != LNF;
-        
     };
     function<ll(int, ll)> dfs = [&](int u, ll flow) -> ll
     {
@@ -52,24 +47,27 @@ pll max_flow(int s, int t, vector<vector<edge> > &e)
         for (int i = cur[u]; i < m && now; ++i)
         {
             cur[u] = i;
-            auto [v, rev, c, w] = e[u][i];
-            if (!c || book[v] || dis[v] != dis[u] + w) continue;
-            ll f = dfs(v, min(now, c));
-            e[u][i].c -= f;
-            e[v][rev].c += f;
-            now -= f;
-            cost += w * f;
+            auto &[v, rev, c, w, edge_flow, fwd] = e[u][i];
+            if (c <= edge_flow || book[v] || dis[v] != dis[u] + w) continue;
+            ll f = dfs(v, min(now, c - edge_flow));
+            if (f > 0)
+            {
+                e[u][i].flow += f;
+                e[v][rev].flow -= f;
+                now -= f;
+                cost += w * f;
+            }
         }
         book[u] = 0;
         return flow - now;
     };
     while (spfa()) ans += dfs(s, LNF);
     return {ans, cost};
-}
+};
 
-加边, c表示流量, w表示费用
+// 加边, c表示流量, w表示费用
 auto addedge = [&](int u, int v, ll c, ll w)
 {
-    e[u].pb({v, (int)e[v].size(), c, w});
-    e[v].pb({u, (int)e[u].size() - 1, 0, -w});
+    e[u].pb({v, (int)e[v].size(), c, w, 0, true});
+    e[v].pb({u, (int)e[u].size() - 1, 0, -w, 0, false});
 };
